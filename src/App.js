990588e3a28915dbd098668c0432181b2e1caffc
Henri Bresteau <henri.bresteau@gamil.com>
2022-04-08 10:46:04 +0200

    map on data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
-import Main from "./Components/Main/Main";
+import Card from "./Components/Card/Card";
 import Navbar from "./Components/Navbar/Navbar";
 
+import data from "./data";
+
 export default function App() {
+  const cards = data.map((item) => {
+    return <Card key={item.id} item={item} />;
+  });
   return (
     <div>
       <Navbar />
-      <Main />
+      <section className="cards-list">{cards}</section>
     </div>
   );
 }
